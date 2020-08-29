aaa5eef11dd402165ca2ce2913b2d8e1a282e4ad
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-08-30 01:58:59 +0900

    11399_ATM 문제
    맞음
    그리디 알고리즘

---
+++ 그리디 알고리즘/11399_ATM.py
@@ -0,0 +1,10 @@
+n = int(input())
+time_list = list(map(int, input().split()))
+time_list.sort()
+sum=0
+wait=0
+for i in range(0, n):
+    sum = sum + (wait + time_list[i])
+    wait = wait + time_list[i]
+
+print(sum)
