b4543a21c61d9042e2b05f419d9dce0837bb50d1
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-09-12 00:21:41 +0900

    1463_1로 만들기
    초기접근 탑다운 방식
    
    해결 -> 바텀업 방식

--- 다이나믹 프로그래밍/1463_1로 만들기.py
+++ 다이나믹 프로그래밍/1463_1로 만들기.py
@@ -1,3 +1,5 @@
+# 1463_1로 만들기
+# 다이나믹 프로그래밍
 
 # 초기 접근 첫번째로 시도했던 코드 (탑다운방식)
 # import math,sys
@@ -28,23 +30,12 @@ import math
 x = int(input())
 n=1
 cnt = 0
-array=[0 for i in range(int(math.pow(10,6))+1)]
-def dynamic(x, cnt, array):
-    if x%3 == 0:
-        if array[int(x / 3)] > cnt+1 or array[int(x / 3)] ==0:
-            array[int(x / 3)] = cnt+1
-            dynamic(x/3, cnt+1, array)
+arr=[0 for i in range(int(math.pow(10,6))+1)]
 
-    if x%2 == 0:
-        if array[int(x / 2)] > cnt+1 or array[int(x / 2)] ==0:
-            array[int(x / 2)] = cnt+1
-            dynamic(x / 2, cnt + 1, array)
-    if x - 1 > 1:
-        if array[int(x - 1)] > cnt + 1 or array[int(x - 1)] ==0:
-            array[int(x - 1)] = cnt + 1
-            dynamic(x - 1, cnt + 1, array)
-
-while n>
-
-dynamic(x, cnt, array)
-print(array[1])
+for i in range(2,x+1):
+    arr[i] = arr[i-1] + 1
+    if i % 2 == 0:
+        arr[i] = min(arr[i], arr[int(i/2)] + 1)
+    if i % 3 == 0:
+        arr[i] = min(arr[i], arr[int(i/3)] + 1)
+print(arr[x])
