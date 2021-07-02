defe3f1bc1c9657a4e1d4fc6668c5074859d9013
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-07-03 00:13:58 +0900

    Update 15652_N과 M (4).py

--- 백트래킹/15652_N과 M (4).py
+++ 백트래킹/15652_N과 M (4).py
@@ -4,3 +4,17 @@ from sys import stdin
 read = stdin.readline
 
 n, m = map(int, read().split())
+
+
+def back(li, s):
+    if len(li) == m:
+        print(" ".join(map(str, li)))
+        return
+
+    for i in range(s, n + 1):
+        li.append(i)
+        back(li, i)
+        li.pop()
+
+
+back([], 1)
