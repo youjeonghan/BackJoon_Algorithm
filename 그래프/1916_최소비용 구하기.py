e21f26e1a6f22c91db5e05ec0103e990187f678b
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-07-13 12:02:56 +0900

    Create 1916_최소비용 구하기.py

---
+++ 그래프/1916_최소비용 구하기.py
@@ -0,0 +1,37 @@
+from sys import stdin
+from collections import defaultdict
+import heapq as hq
+
+read = stdin.readline
+
+city = int(read())
+bus = int(read())
+
+vertex ={i:float("inf") for i in range(1, city+1)}
+edge = defaultdict(list)
+visit = [0] * (city+1)
+heap = []
+
+for _ in range(bus):
+    s,e,w = map(int, read().split())
+    edge[s].append((e,w))
+
+
+
+start, end = map(int, read().split())
+
+vertex[start] = 0
+hq.heappush(heap, (0, start))
+
+while heap:
+    w, node = hq.heappop(heap)
+    if vertex[node] <w:
+        continue
+    for n_v, n_w in edge[node]:
+        if vertex[n_v] > w + n_w:
+            vertex[n_v] = w + n_w
+            hq.heappush(heap, (vertex[n_v], n_v))
+    
+print(vertex[end])
+
+
