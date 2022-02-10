d4bb39919532b2c731ba8c9349b525cc76abe2ef
coding_egg <yjh9360@naver.com>
2022-02-11 00:06:10 +0900

    1753_최단경로 불필요한 코드 제거

--- 다익스트라/1753_최단경로.py
+++ 다익스트라/1753_최단경로.py
@@ -17,18 +17,14 @@ for _ in range(E):
     edge[u].append((v, w))
 
 vertex[K] = 0
-order = 1
-visit = [0] * (V + 1)
 heap = []
 hq.heappush(heap, (0, K))
 while heap:
     node = hq.heappop(heap)
-    visit[node[1]] = 1
     for v, w in edge[node[1]]:
         if vertex[v] > node[0] + w:
             vertex[v] = node[0] + w
-            if not visit[v]:
-                hq.heappush(heap, (vertex[v], v))
+            hq.heappush(heap, (vertex[v], v))
 
 
 for k, v in vertex.items():
